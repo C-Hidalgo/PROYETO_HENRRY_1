9b32ce55ff6a49297ca900ad0b5e2e4965a5069a
C-Hidalgo <hidalgo.carlos1984@gmail.com>
2023-12-28 19:13:17 -0300

    actual

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import uvicorn
 app = FastAPI()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=10000, debug=True)
 
 
 @app.get('/')
